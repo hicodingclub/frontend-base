ee35d18995a779a680861cf8dc12b4a719d88182
Xibin Liu <xibinliu@xibin-mac.local>
2020-07-03 15:59:29 -0700

    upgrade 0.9.15

--- front-adm/src/app/injection-tokens/auth.conf.ts
+++ front-adm/src/app/injection-tokens/auth.conf.ts
@@ -4,8 +4,8 @@ export const authentication_page_root_uri: string = '/auth';
 export const authentication_server_root_uri: string = '/api/auth';
 export const authentication_interfaces: string = "admin";
 export const authentication_login_pipeline = undefined;
-export const authentication_registration_pipeline = undefined; // string
-export const authentication_registration_required = undefined; // {firstName: false, lastName: false, phone: false}
+export const authentication_registration_pipeline = ''; // string
+export const authentication_registration_required = {firstName: false, lastName: false, phone: false};
 
 export const authentication_dropdown_items: DropdownItem[] = [
     // {displayName: 'My classes', routerLink: '/academics/class'},
